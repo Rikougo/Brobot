fc58e6ef8ae494dffd66b04ca19a6084718da039
Elye <samhelye@gmail.com>
2022-10-04 13:08:14 +0200

    Moved visual assets

---
+++ Assets/Scripts/GameAction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class GameAction
+{
+    private Dictionary<string, int> m_preconditions;
+    private Dictionary<string, int> m_postconditions;
+
+    private string m_title;
+    
+    
+}
