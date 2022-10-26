c7743866721e060b2109f4be2e30548d3769edc9
Elye <samhelye@gmail.com>
2022-10-26 21:19:13 +0200

    Fixed camera based move and asset bind on first level

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -14,55 +14,59 @@ namespace Controllers
     {
         private PlayerInput m_input;
         private Entity m_entity;
-        private Transform m_mainCam;
         [SerializeField] private TextMeshProUGUI m_hintText;
 
         private List<Actionable> m_actionsInRange;
-        
+        private Transform m_mainCam;
+        private Vector2 m_camForward;
+        private Vector2 m_camRight;
+
+        private bool m_started;
+
         private void Awake()
         {
             m_input = GetComponent<PlayerInput>();
             m_entity = GetComponent<Entity>();
             m_actionsInRange = new List<Actionable>();
+
+            m_started = false;
         }
-        
-        public void Start()
+
+        private void Start()
         {
-            m_mainCam = GameObject.FindWithTag("MainCamera").transform;
-            Vector2 l_forward = new Vector2(m_mainCam.forward.x, m_mainCam.forward.z);
-            Vector2 l_right = new Vector2(m_mainCam.right.x, m_mainCam.right.z);
-
-            m_hintText.gameObject.SetActive(false);
-            
-            m_input.actions["Move"].performed += (p_ctx) =>
-            {
-                Vector2 l_inputAxis = p_ctx.ReadValue<Vector2>();
-                m_entity.Direction = l_inputAxis.y * l_forward + l_inputAxis.x * l_right;
-            };
-
-            m_input.actions["Move"].canceled += (_) =>
-            {
-                m_entity.Direction = Vector2.zero;
-            };
-
-            m_input.actions["Interact"].performed += (_) =>
-            {
-                if (m_actionsInRange.Count > 0)
-                {
-                    m_actionsInRange.Last().DoAction(m_entity);
-                }
-            };
-            
-            m_input.actions["CamSwitch"].performed += (p_ctx) => {
-                GameObject.FindWithTag("GameController").GetComponent<GameDirector>().NextCamera((int)p_ctx.ReadValue<float>());
-            };
-            
-            m_input.actions["Help"].performed += (p_ctx) => {
-                GameObject.FindObjectOfType<UILogger>().PrintHelp();
-            };
+            m_started = true;
+            OnStartOrEnable();
         }
 
-        public void OnTriggerEnter(Collider p_other)
+        private void OnEnable() { OnStartOrEnable(); }
+
+        private void OnDisable()
+        {
+            m_input.actions["Move"].performed -= Move;
+            m_input.actions["Move"].canceled -= Move;
+
+            m_input.actions["Interact"].performed -= Interact;
+            m_input.actions["CamSwitch"].performed -= CamSwitch;
+            m_input.actions["Help"].performed -= PrintHelp;
+        }
+
+        private void OnStartOrEnable()
+        {
+            if (!m_started) return;
+            
+            m_mainCam = GameObject.FindWithTag("MainCamera").transform;
+            UpdateForward();
+            m_hintText.gameObject.SetActive(false);
+
+            m_input.actions["Move"].performed += Move;
+            m_input.actions["Move"].canceled += Move;
+
+            m_input.actions["Interact"].performed += Interact;
+            m_input.actions["CamSwitch"].performed += CamSwitch;
+            m_input.actions["Help"].performed += PrintHelp;
+        }
+
+        private void OnTriggerEnter(Collider p_other)
         {
             Actionable l_action = p_other.GetComponent<Actionable>();
             if (l_action != null)
@@ -75,10 +79,9 @@ namespace Controllers
                     m_hintText.gameObject.SetActive(true);
                 }
             }
-            
         }
 
-        public void OnTriggerExit(Collider p_other)
+        private void OnTriggerExit(Collider p_other)
         {
             Actionable l_action = p_other.GetComponent<Actionable>();
             if (l_action != null)
@@ -89,5 +92,48 @@ namespace Controllers
                 m_hintText.gameObject.SetActive(m_actionsInRange.Count > 0);
             }
         }
+
+        private void UpdateForward()
+        {
+            Vector3 l_camToPlayerDirection = (transform.position - m_mainCam.position).normalized;
+            Vector3 l_right = Vector3.Cross(Vector3.up, l_camToPlayerDirection);
+            m_camForward = new Vector2(l_camToPlayerDirection.x, l_camToPlayerDirection.z).normalized;
+            m_camRight = new Vector2(l_right.x, l_right.z).normalized;
+        }
+
+        #region INPUT_ACTION_METHOD
+        private void Move(InputAction.CallbackContext p_ctx)
+        {
+            if (p_ctx.performed)
+            {
+                UpdateForward();
+                Vector2 l_inputAxis = p_ctx.ReadValue<Vector2>();
+                m_entity.Direction = l_inputAxis.y * m_camForward + l_inputAxis.x * m_camRight;
+            }
+            else if (p_ctx.canceled)
+            {
+                m_entity.Direction = Vector2.zero;
+            }
+        }
+
+        private void Interact(InputAction.CallbackContext p_ctx)
+        {
+            if (m_actionsInRange.Count > 0)
+            {
+                m_actionsInRange.Last().DoAction(m_entity);
+            }
+        }
+
+        private void CamSwitch(InputAction.CallbackContext p_ctx)
+        {
+            GameObject.FindWithTag("GameController").GetComponent<GameDirector>()
+                .NextCamera((int)p_ctx.ReadValue<float>());
+        }
+
+        private void PrintHelp(InputAction.CallbackContext p_ctx)
+        {
+            GameObject.FindObjectOfType<UILogger>().PrintHelp();
+        }
+        #endregion
     }
 }
